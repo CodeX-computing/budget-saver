b1c0ae4ba2e6a2aef34f2ab2f5e36ea710b36d55
Ismail Courr <contact@ismailcourr.ml>
2022-08-08 12:54:28 +0100

    Create the migrations using scaffold

---
+++ db/migrate/20220808115035_create_groups.rb
@@ -0,0 +1,12 @@
+class CreateGroups < ActiveRecord::Migration[7.0]
+  def change
+    create_table :groups do |t|
+      t.string :icon
+      t.string :name
+      t.references :User, null: false, foreign_key: true
+      t.references :conversion, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
