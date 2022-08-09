ff1b740aad2a633070638d784ee405c65458886b
Ismail Courr <contact@ismailcourr.ml>
2022-08-09 10:37:07 +0100

    Fix relations & modify message

--- app/models/conversion.rb
+++ app/models/conversion.rb
@@ -1,3 +1,4 @@
 class Conversion < ApplicationRecord
   belongs_to :User
+  has_many :group
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,4 @@
 class Group < ApplicationRecord
   belongs_to :User
-  belongs_to :conversion
+  has_many :Conversion
 end
