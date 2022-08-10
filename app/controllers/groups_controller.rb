f7a75fe79d0203db9df19c5049145628fecfa822
Ismail Courr <contact@ismailcourr.ml>
2022-08-10 11:04:37 +0100

    Fix user access

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
   # GET /groups or /groups.json
   def index
-    @groups = Group.all
+    @groups = current_user.groups.all
   end
 
   # GET /groups/1 or /groups/1.json
