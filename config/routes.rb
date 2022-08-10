f0e7d22d02bf6ffcef92bc993bf9fbcfc0381de8
Ismail Courr <contact@ismailcourr.ml>
2022-08-10 12:54:57 +0100

    Fix sign up root

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'splash#index'
-  # resources :conversions
+  get '/users/:id', to: 'groups#index', as: 'user_root'
   resources :groups do
     resources :conversions
     resources :group_conversions
   end
   resources :users
+  # resources :conversions
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
