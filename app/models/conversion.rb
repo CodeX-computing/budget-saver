d2501784b558fac6ef6b15d0c47eb6941f05cdd8
Ismail Courr <contact@ismailcourr.ml>
2022-08-10 10:24:49 +0100

    Add total amount of transactions & validations

--- app/models/conversion.rb
+++ app/models/conversion.rb
@@ -3,5 +3,5 @@ class Conversion < ApplicationRecord
   has_many :group_conversions, class_name: 'GroupConversion', foreign_key: :conversion_id
 
   validates :name, presence: true, length: { maximum: 255 }
-  validates :amount, presence: true
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
