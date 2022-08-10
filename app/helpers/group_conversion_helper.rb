d59268f29ca26f7057e2dc6b2cf84a27d4ab23f8
Ismail Courr <contact@ismailcourr.ml>
2022-08-10 13:43:10 +0100

    reset

--- app/helpers/group_conversion_helper.rb
+++
@@ -1,2 +0,0 @@
-module GroupConversionHelper
-end
--- test/controllers/group_conversion_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class GroupConversionControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
